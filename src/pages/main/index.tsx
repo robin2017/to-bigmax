44d64c7843feca5cfe92899660e60ef3c2f4181f
伯他 <bota.lb@alibaba-inc.com>
2022-01-30 17:25:50 +0800

    auto save

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -3,6 +3,7 @@ import View from 'rax-view';
 import { Swiper, SwiperSlide } from 'rax-swiper';
 import Image from 'rax-image';
 import styles from './index.module.css';
+import {initVars,frame} from '../../utils/index';
 export default () => {
   const ref = useRef(null);
 
@@ -18,8 +19,15 @@ export default () => {
     // {url:'ddd',content:'55'}
   ];
 
+  useEffect(() => {
+ 
+   initVars();
+     frame();
+  }, []);
   return (
     <View style={{ height: '100%' }}>
+      <canvas className={styles.canvas1} id="canvas"></canvas>
+      <canvas className={styles.canvas2}></canvas>
       <Swiper onSlideChange={(e) => console.log(e)} ref={ref} className={styles.Swiper} autoplay={true}>
         {imageList.map((image, index) => {
           return (
@@ -27,7 +35,10 @@ export default () => {
               <View style={{}}>
                 这是标题
                 <View className={styles.ImageWrapper}>
-                  <Image source={{ uri: image.url, width: '750rpx', marginTop: image.marginTop,  }} className={styles.Image} />
+                  <Image
+                    source={{ uri: image.url, width: '750rpx', marginTop: image.marginTop }}
+                    className={styles.Image}
+                  />
                 </View>
               </View>
             </SwiperSlide>
